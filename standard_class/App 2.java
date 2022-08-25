46ea1db831226fa4f1a496a406af1c2e101d67ef
urielgeianbesa <58388385+urielgeianbesa@users.noreply.github.com>
2022-08-25 15:58:03 +0800

    Add files via upload

---
+++ standard_class/App 2.java
@@ -0,0 +1,46 @@
+public class App {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // Creates a new object named "Assassin" from Character class
+        Character Assassin  = new Character(5, 10, 10, "Ling");
+        // Calls the sayMyName method
+        Assassin .sayMyName();
+        Assassin .sayMyattribute();
+
+
+        // Creates a new object named "Tank" from Character class
+        Character Tank  = new Character(10, 2, 1, "Grock");
+        // Calls the sayMyName method
+        Tank .sayMyName();
+        Tank .sayMyattribute();
+
+        // Creates a new object named "mage" from Character class
+        Character Mage  = new Character(3, 2, 10, "Rafaela");
+        // Calls the sayMyName method
+        Mage .sayMyName();
+        Mage .sayMyattribute();
+
+         // Creates a new object named "Fighter " from Character class
+         Character Fighter   = new Character(10, 7, 5, "Alucard");
+         // Calls the sayMyName method
+         Fighter  .sayMyName();
+         Fighter  .sayMyattribute();
+
+        // Creates a new object named "Support  " from Character class
+           Character Support    = new Character(3, 3, 10, "Diggie");
+        // Calls the sayMyName method
+        Support   .sayMyName();
+        Support   .sayMyattribute();
+
+
+ 
+
+
+    }
+}
+
+
+    
---
+++ standard_class/Character 2.java
@@ -0,0 +1,29 @@
+public class Character {
+    // Attributes
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    // Method
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    
+    }
+
+    // Constructor
+    public Character(int str, int agi, int intel, String characterName) {
+        strength = str;
+        agility = agi;
+        intelligence = intel;
+        name = characterName;
+    }
+
+        public void sayMyattribute() {
+            System.out.println("str:" + strength + "\nagi:" + agility + "\nintel:" + intelligence);
+
+
+
+
+    }
+}
