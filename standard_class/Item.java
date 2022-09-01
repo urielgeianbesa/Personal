c9807162abe26208da0cbdffe5c1128969d8b699
urielgeianbesa <58388385+urielgeianbesa@users.noreply.github.com>
2022-09-01 15:58:53 +0800

    Add files via upload

---
+++ standard_class/Item.java
@@ -0,0 +1,30 @@
+public class Item {
+
+      // Item Sop
+      int  object;
+      int Price;
+      byte Weight;
+      byte  Quantity;
+      int  Description;
+      boolean  Canbetraded; 
+
+        // Item Shop
+        // object healingpotion
+        // Price - 50
+        // Weight - 10
+        // Quantity - 10 
+        // Description - "this will heal for 10 hp"
+        // Canbetraded - false 
+
+        // object Armor
+        // Price - 250
+        // Weight - 30
+        // Quantity - 1
+        // Description - "this will gives you plus 50 in defense"
+        // Canbetraded - true
+
+
+
+
+    
+}
---
+++ standard_class/Pets.java
@@ -0,0 +1,46 @@
+public class Pets {
+    // Pets
+      int object;
+      int Name;
+      byte Petlevel;
+      boolean  ishappy;
+      int Damage;
+      boolean Canbetraded; 
+
+       // Pets :
+    // object - Dog
+    // Name - Yokyok
+    // Petlevel - 10
+    // ishappy - true
+    // Damage - 25
+    // Canbreed - true
+
+        // Pets :
+    // object - Cat
+    // Name - Nyaw nyaw
+    // Petlevel - 30
+    // ishappy - true
+    // Damage - 15
+    // Canbreed - true
+
+        // Pets :
+    // object - Pokemon
+    // Name - Snorlax
+    // Petlevel - 58
+    // ishappy - true
+    // Damage - 82
+    // Canbreed - False
+
+        // Pets :
+    // object - Monkey
+    // Name - Jimboy
+    // Petlevel - 28
+    // ishappy - False
+    // Damage - 18
+    // Canbreed - False
+
+
+    
+
+
+}
